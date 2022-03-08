340900090630f0d23124a75d6322ef5a4d38136a
sharifulislm <shariful.islam.coder@gmail.com>
2022-03-08 21:40:51 +0300

    added Revision

---
+++ all-ES6/keys-lop.js
@@ -0,0 +1,6 @@
+// keys, values
+const keys = Object.keys(student);
+const values = Object.values(student);
+
+// for of on array like object
+// loop on an object using for in 
--- fetch-joson.js
+++ fetch-joson.js
@@ -5,9 +5,9 @@ const student = {
     age: 32,
     movies: ['king khan', 'Dhakar Mastan']
 }
-// const student =[ 'sharifu' , 'soborna ' ,{name: 'arif', age: '20', frome: 'india'}, ' taizul islam' , 'Afrin'];
+const student =[ 'sharifu' , 'soborna ' ,{name: 'arif', age: '20', frome: 'india'}, ' taizul islam' , 'Afrin'];
 // Object k JSON a korte stringify use korte hoy 
-// const studentJSON = JSON.stringify(student[2].frome);
+const studentJSON = JSON.stringify(student[2].frome);
 
 
 
@@ -18,4 +18,41 @@ const studentJSON1 = JSON.parse(studentJSON);
 
 console.log(studentJSON);
 console.log(studentJSON1);
-// console.log(student);
+console.log(student);
+
+//2. fetch
+fetch('url')
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+
+
+// for 
+const numbers = [23, 54, 67, 87, 23, 78];
+// numbers.forEach(num => console.log(num));
+numbers.map(num => num * 2);
+
+
+
+
+// add or remove from an array
+const products = [
+    { name: 'laptop', price: 3200, brand: 'len', color: 'silver' },
+    { name: 'phone', price: 7000, brand: 'HTC', color: 'golden' },
+    { name: 'watch', price: 3000, brand: 'casio', color: 'yellow' },
+    { name: 'sungalss', price: 300, brand: 'ray', color: 'black' },
+    { name: 'camera', price: 9000, brand: 'canon', color: 'gray' }
+];
+// create a new array without one specific item 
+const newProduct = { name: 'webcam', price: 700, brand: 'Lal' };
+
+// copy products array and then add newProduct 
+const valueOfproducts = [...products,newProduct];
+// console.log(valueOfproducts);
+
+// remove phone means create a new array without the phone
+const filterofobject = products.filter(n => n.name !=='phone');
+console.log(filterofobject);
+
+
+
